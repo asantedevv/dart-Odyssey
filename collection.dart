75f4cd3cbd03600cff701b843214749fb4cb2224
Kwame Asante Junior <vie97pie@gmail.com>
2024-09-19 09:07:08 +0000

    list, set, and map collections

---
+++ collection.dart
@@ -0,0 +1,21 @@
+// Lists
+var tools = ['sprayer', 'axe', 'rake', 'shovel'];
+
+
+// Sets
+var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+var names = <String>{}; // creating an empty set
+
+void main() {
+  print(tools.length); // get a list's length using the .length property
+  print(halogens.length); // get a set's length using the . length property
+}
+
+
+// Map
+var countries = {
+  // Key:    Value
+  'Doha': 'Qatar',
+  'Madrid': 'Spain',
+  'Oslo': 'Norway'
+};
